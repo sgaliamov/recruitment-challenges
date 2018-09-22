10023b68280c56b8e8701286f8d2791e9d3a0f09
s. galiamov <sgalyamov@gmail.com>
2018-09-22 23:30:41 +0200

    more optimizations

--- Algorithms.CountingBits/OptimizedBitwisePositiveBitCounter.cs
+++ Algorithms.CountingBits/OptimizedBitwisePositiveBitCounter.cs
@@ -14,41 +14,51 @@ namespace Payvision.CodeChallenge.Algorithms.CountingBits
     /// </summary>
     public sealed class OptimizedBitwisePositiveBitCounter : IPositiveBitCounter
     {
+        private const int M1 = 0x55555555; //binary: 0101...
+        private const int M2 = 0x33333333; //binary: 00110011..
+        private const int M4 = 0x0f0f0f0f; //binary:  4 zeros,  4 ones ...
+
         public IEnumerable<int> Count(int input)
         {
             if (input < 0)
             {
-                throw new ArgumentException(nameof(input));
+                throw new ArgumentException("Parameter must be a positive integer.", nameof(input));
             }
 
-            return GetCount(input);
+            return GetCountData(input);
         }
 
-        private static IEnumerable<int> GetCount(int input)
+        private static int GetBitCount(int input)
         {
-            const int maxCapacity = sizeof(int) * 8 - 1;
-            var positions = new Stack<int>(maxCapacity);
+            input -= (input >> 1) & M1; //put count of each 2 bits into those 2 bits
+            input = (input & M2) + ((input >> 2) & M2); //put count of each 4 bits into those 4 bits
+            input = (input + (input >> 4)) & M4; //put count of each 8 bits into those 8 bits
+            input += input >> 8; //put count of each 16 bits into their lowest 8 bits
+            input += input >> 16; //put count of each 32 bits into their lowest 8 bits
 
-            var index = 1;
-            var marker = 0b01000000_00000000_00000000_00000000;
+            return input & 0x7f;
+        }
 
-            while (marker != 0)
+        private static IEnumerable<int> GetCountData(int input)
+        {
+            yield return GetBitCount(input);
+
+            foreach (var position in GetSwitchedBitPositions(input))
             {
-                var bit = input & marker;
-                if (bit > 0)
-                {
-                    positions.Push(index);
-                }
-
-                marker >>= 1;
-                index++;
+                yield return position;
             }
+        }
 
-            yield return positions.Count;
+        private static IEnumerable<int> GetSwitchedBitPositions(int input)
+        {
+            var iterator = 0;
 
-            while (positions.Count != 0)
+            for (var i = 1; i <= (0b01000000_00000000_00000000_00000000) && i > 0; i <<= 1, iterator++)
             {
-                yield return maxCapacity - positions.Pop();
+                if ((input & i) > 0)
+                {
+                    yield return iterator;
+                }
             }
         }
     }
